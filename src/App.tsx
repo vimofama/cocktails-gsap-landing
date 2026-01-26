644e1eac2d1a67d46f4c3754d3c8e8abc003e654
Mateo <vimofama@outlook.com>
2026-01-26 14:59:51 -0500

    add About component with GSAP animations and content

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from 'gsap/all';
 import Navbar from "./components/Navbar.tsx";
 import Hero from "./components/Hero.tsx";
 import Cocktails from "./components/Cocktails.tsx";
+import About from "./components/About.tsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -12,6 +13,7 @@ const App = () => {
             <Navbar />
             <Hero />
             <Cocktails />
+            <About />
         </main>
     );
 };
